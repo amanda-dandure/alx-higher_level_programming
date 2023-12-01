0794a2f2688559351f4ad6bd1989be2d5b892399
amanda-dandure <amandadandure2@gmail.com>
2023-12-01 00:42:39 -0800

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if num == 1:
         print("{} arguments.".format(num - 1))
     elif num == 2:
-        print("{} arguments.".format(num - 1))
+        print("{} argument.".format(num - 1))
     else:
         print("{} arguments.".format(num - 1))
 
