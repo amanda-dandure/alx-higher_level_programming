20a4658c20e7d6e5fe88354d297754b8cc416b5a
amanda-dandure <amandadandure2@gmail.com>
2024-01-09 08:51:56 -0800

    a function that creates an Object from a “JSON file”

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Defining a JSON file reading function """
+import json
+
+
+def load_from_json_file(filename):
+    """ Creating a python object from a JSON file """
+    with open(filename) as f:
+        return json.load(f)
