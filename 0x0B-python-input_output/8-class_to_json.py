cb60a3ea821a7bbcba14bcfd630949b6f6870904
amanda-dandure <amandadandure2@gmail.com>
2024-01-09 09:42:12 -0800

    class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Defining a python class-to-JSON func """
+
+
+def class_to_json(obj):
+    """ Returns the dictonary represenntation of a simple data structure """
+    return.__dict__
